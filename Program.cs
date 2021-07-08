91a8e4fb4f15dc220145df1c72834b260075d9e4
burak oranlier <oranlier@hotmail.com>
2021-07-08 13:40:31 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Switch_Case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month =DateTime.Now.Month ;
+
+            switch (month)//expression
+            {
+                case 1:
+                    Console.WriteLine("Ocak ayındasınız");
+                    break;
+                case 2:
+                    Console.WriteLine("Şubat ayındasınız");
+                    break;            
+                case 3:
+                    Console.WriteLine("Mart ayındasınız");
+                    break;
+                case 4:
+                    Console.WriteLine("nisan ayındasınız");
+                    break;
+                case 5:
+                    Console.WriteLine("mayıs ayındasınız");
+                    break;
+                case 6:
+                    Console.WriteLine("haziran ayındasınız");
+                    break;
+                case 7:
+                    Console.WriteLine("temmuz ayındasınız");
+                    break;
+                case 8:
+                    Console.WriteLine("agustos ayındasınız");
+                    break;
+                case 9:
+                    Console.WriteLine("eylul ayındasınız");
+                    break;            
+                case 10:
+                    Console.WriteLine("ekim ayındasınız");
+                    break;
+                case 11:
+                    Console.WriteLine("kasım ayındasınız");
+                    break;
+                case 12:
+                    Console.WriteLine("aralık ayındasınız");
+                    break;
+                default://hicbir case e uymuyorsa yapılacak ifade
+                    Console.WriteLine("Yanlış veri girişi");
+                break;
+            }
+
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış Ayındasınız!");
+                    break;
+                default:
+                    Console.WriteLine("Kış Ayında değilsiniz!");
+                    break;
+            }
+        }
+    }
+}
